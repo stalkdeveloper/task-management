e4a5d596fb94d2104d19589e8601ae77973e20e0
stalkdeveloper <91655988+stalkdeveloper@users.noreply.github.com>
2024-10-19 17:57:08 +0530

    changes has been done

---
+++ lang/en/cruds.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'datatable' => [
+        'show' => 'Show',
+        'entries' => 'entries',
+        'showing' => 'Showing',
+        'to' => 'to',
+        'of'    => 'of',
+        'search' => 'Search',
+        'previous' => 'Previous',
+        'next' => 'Next',
+        'first' => 'First',
+        'last'  => 'Last',
+        'data_not_found' => 'Data not found',
+        'processing'   => 'Processing...',
+    ],
+
+    'dashboard' =>  [
+        'title'          => 'Dashboard',
+        'title_singular' => 'Dashboard',
+        'fields'         => [
+            
+        ],
+    ],
+
+    'header' =>  [
+        'title'          => 'Header',
+        'title_singular' => 'Header',
+        'fields'         => [
+            'notifications'         =>  'Notifications',
+        ],
+    ],
+
+    'menus' => [
+        'dashboard' => 'Dashboard',
+        'setting' => 'Settings',
+        'user' => "Users",
+        'task'  => 'Tasks',
+    ],
+
+    'task' =>  [
+        'title'                      => 'Tasks',
+        'title_singular'        => 'Task',
+        'fields'                        => [
+            'title'                           =>  'Title',
+            'description'               =>  'Description',
+            'due_date'                  =>  'Due Date',
+            'status'                       =>  'Status',
+        ],
+    ],
+
+];
